989f7a741b9c53ba729864fb80f71daf4a723b0e
veinhorn <mcklas@mail.ru>
2014-02-08 19:10:28 +0300

    Added sample.

---
+++ src/forismatic/Sample.java
@@ -0,0 +1,23 @@
+package forismatic;
+
+import forismatic.Forismatic.Quote;
+
+public class Sample {
+	public static void main(String[] args) {
+		Quote quote = null;
+		for(int i = 0; i < 5; i++) {
+			if(i % 2 == 0)
+				quote = new Forismatic(Forismatic.RUSSIAN).getQuote();
+			else {
+				quote = new Forismatic(Forismatic.ENGLISH).getQuote();
+			}
+			System.out.println(quote.getQuoteText());
+			System.out.println(quote.getQuoteAuthor());
+			try {
+				Thread.sleep(3000);
+			} catch(InterruptedException e) {
+						
+			}
+		}
+	}
+}
