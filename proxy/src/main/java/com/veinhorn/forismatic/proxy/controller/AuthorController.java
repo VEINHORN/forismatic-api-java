8053455379a753e7392bb4d1130885ed8e2a7bde
Boris Korogvich <b.korogvich@gmail.com>
2022-03-20 11:47:38 +0300

    Add support for sorting by name field

--- proxy/src/main/java/com/veinhorn/forismatic/proxy/controller/AuthorController.java
+++ proxy/src/main/java/com/veinhorn/forismatic/proxy/controller/AuthorController.java
@@ -5,6 +5,7 @@ import com.veinhorn.forismatic.proxy.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,6 @@ public class AuthorController {
     @GetMapping("/authors")
     public Page<AuthorDto> getAllAuthors(@RequestParam(name = "page", defaultValue = "0") Integer page,
                                          @RequestParam(name = "size", defaultValue = "5") Integer size) {
-        return service.getAllAuthors(PageRequest.of(page, size));
+        return service.getAllAuthors(PageRequest.of(page, size).withSort(Sort.by("name")));
     }
 }
