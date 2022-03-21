1d133c0e4b52214559294fe942037109a7a2a71a
Boris Korogvich <b.korogvich@gmail.com>
2022-03-21 18:05:20 +0300

    Move SpringFox configuration to the configuration package

--- proxy/src/main/java/com/veinhorn/forismatic/proxy/configuration/SpringFoxConfig.java
+++ proxy/src/main/java/com/veinhorn/forismatic/proxy/configuration/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.veinhorn.forismatic.proxy;
+package com.veinhorn.forismatic.proxy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
